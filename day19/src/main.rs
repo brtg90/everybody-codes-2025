4ed59eb8fb18e185af5209e7a6822ef58ee3a5a1
brtg90 <bartgroeneveld1@hotmail.com>
2025-12-01 21:36:20 +0100

    Day19 pt 1

--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, VecDeque};
+
 use utils::read_lines;
 
 fn main() {
@@ -9,7 +11,8 @@ fn main() {
 fn part_1() {
     let input = parse("inputs/day19pt1.txt");
     println!("{:?}", input);
-    let flaps = count_flaps_for_triplets(&input, 0, 0);
+    // let flaps = count_flaps_for_triplets(&input, 0, 0);
+    let flaps = bfs(&input, 0, 0);
     println!("Part 1: {:?}", flaps);
 }
 
@@ -21,51 +24,63 @@ fn part_3() {
     println!("Part 3: {:?}", 0);
 }
 
-fn count_flaps_for_triplets(triplets: &Vec<Vec<isize>>, x: isize, y: isize) -> isize {
-    let mut flaps = 0;
+fn bfs(triplets: &Vec<Vec<isize>>, x: isize, y: isize) -> isize {
+    let mut queue = VecDeque::new();
+    queue.push_back((x, y, 0, 0));
 
-    let options = take_obstacle(&triplets[0], x, y);
-    if options.is_none() {
-        return 0;
-    }
+    // Track best flaps for each (x, y, triplet_num) state
+    let mut best: HashMap<(isize, isize, usize), isize> = HashMap::new();
+    let mut min_flaps = isize::MAX;
 
-    for option in options.unwrap() {
-        if triplets.len() > 1 {
-            flaps += option[2] + count_flaps_for_triplets(&triplets[1..].to_vec(), option[0], option[1]);
-
+    while let Some((x, y, flaps, triplet_num)) = queue.pop_front() {
+        let state = (x, y, triplet_num);
+        if let Some(&prev_best) = best.get(&state) {
+            if flaps > prev_best {
+                continue;
+            }
         }
-        else {
-            return flaps;
+        best.insert(state, flaps);
+
+        if triplet_num == triplets.len() {
+            min_flaps = min_flaps.min(flaps);
+            continue;
+        }
+
+        if let Some(options) = take_obstacle(&triplets[triplet_num], x, y) {
+            for option in options {
+                queue.push_back((option[0], option[1], flaps + option[2], triplet_num + 1));
+            }
         }
     }
-    flaps
+    min_flaps
 }
+
+
 
 fn take_obstacle(obstacle: &[isize], x: isize, y: isize) -> Option<Vec<Vec<isize>>> {
     let dx = obstacle[0] - x;
-    let (dy_max, dy_min) = get_max_min(obstacle[1] - y, obstacle[1] - y + obstacle[2]);
-    println!("dx {dx}, dy_max {dy_max}, dy_min {dy_min}");
+    // Each step in x-direction is either a flap or not (i.e. difference of two in height if one
+    // is swapped for the other)
+    let y_max = dx + y;
+    let y_min = -dx + y;
 
-    let possible: Vec<bool> = (dy_min..=dy_max)
-        .map(|dy| (dy / dx).abs() <= 1)
+    let obstacle_opening: Vec<isize> = (obstacle[1]..obstacle[1] + obstacle[2])
         .collect();
 
-    if !possible.contains(&true) {
-        // Point out of reach
+    let possible: Vec<(usize, isize)> = (y_min..=y_max).step_by(2)
+        .enumerate()
+        .filter(|(_, y_new)| obstacle_opening.contains(y_new))
+        .collect();
+
+    if possible.is_empty() {
         return None;
     }
 
     let possible_end_points = possible.iter()
-        .enumerate()
-        .filter(|(_, possible)| **possible)
-        .map(|(i, _)| vec![obstacle[0], dy_min + i as isize, std::cmp::max(0, dy_min + i as isize - y)])
+        .map(|(i, y_new)| vec![obstacle[0], *y_new, *i as isize])
         .collect();
 
     Some(possible_end_points)
-}
-
-fn get_max_min(a: isize, b: isize) -> (isize, isize) {
-    (std::cmp::max(a, b), std::cmp::min(a, b))
 }
 
 fn parse(filename: &str) -> Vec<Vec<isize>> {
